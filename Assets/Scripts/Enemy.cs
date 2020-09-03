1051d64084179893fa5b88a2cfe196984a3de11a
Diana Ovod <ovod.diana@gmail.com>
2020-09-02 20:15:43 -0700

    Enemy animations and checkpoint created

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int target = 0;
+    public Transform exitPoint;
+    public Transform[] waypoints;
+    public float navigationUpdate;
+
+    private Transform enemy;
+    private float navigationTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waypoints != null)
+        {
+            navigationTime += Time.deltaTime;
+            if (navigationTime > navigationUpdate)
+            {
+                if (target < waypoints.Length)
+                {
+                    enemy.position = Vector2.MoveTowards(enemy.position, waypoints[target].position, navigationTime);
+                }
+                else
+                {
+                    enemy.position = Vector2.MoveTowards(enemy.position, exitPoint.position, navigationTime);
+                }
+                navigationTime = 0;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "checkpoint")
+            target += 1;
+        else if (other.tag == "Finish")
+        {
+            GameManager.instance.RemoveEnemyFromScreen();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public GameObject spawnPoint;
+    public GameObject [] enemies;
+    public int maxEnemiesOnScreen;
+    public int totalEnemies;
+    public int enemiesPerSpawn;
+
+    private int enemiesOnScreen = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemy();
+
+    }
+
+    void SpawnEnemy()
+    {
+        if (enemiesPerSpawn <= 0 || enemiesOnScreen >= totalEnemies)
+            return;
+
+        for(int i = 0; i < enemiesPerSpawn; i++)
+        {
+            if (enemiesOnScreen >= maxEnemiesOnScreen)
+                break;
+
+            GameObject newEnemy = Instantiate(enemies[0]);
+            newEnemy.transform.position = spawnPoint.transform.position;
+            enemiesOnScreen += 1;
+        }
+    }
+
+    public void RemoveEnemyFromScreen()
+    {
+        if (enemiesOnScreen > 0)
+            enemiesOnScreen -= 1;
+    }
+}
